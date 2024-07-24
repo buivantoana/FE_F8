a8b332b409e7f7d3188ec9f7504518b5a3c3c61f
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-07-24 21:20:51 +0700

    updating state

--- src/pages/client/wite_blog_post/WiteBlogPostController.tsx
+++ src/pages/client/wite_blog_post/WiteBlogPostController.tsx
@@ -6,7 +6,11 @@ import { toast } from "react-toastify";
 import Progress from "@/components/Process";
 
 const WiteBlogPostController = () => {
-
+    const [content, setContent] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [file, setFile] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [showProgress, setShowProgress] = useState(false);
 }
 
 export default WiteBlogPostController;
