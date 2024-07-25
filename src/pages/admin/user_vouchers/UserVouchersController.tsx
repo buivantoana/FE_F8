036236bfe6b9a72bed2e7c91f56c9972a2e89b7f
danglhph30148 <danglhph30148@fpt.edu.vn>
2024-07-25 22:16:05 +0700

    uservoucherscontroller

--- src/pages/admin/user_vouchers/UserVouchersController.tsx
+++ src/pages/admin/user_vouchers/UserVouchersController.tsx
@@ -32,7 +32,9 @@ const UserVouchersController = () => {
     setDeleteCategory(dataDelete);
     setAnchorEl(event.currentTarget);
   };
- 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
 
