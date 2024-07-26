407ea8c6d2e8229812b85f450122700d67ed61fd
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-07-26 21:09:07 +0700

    create props

--- src/pages/client/wite_blog_post/WiteBlogPostView.tsx
+++ src/pages/client/wite_blog_post/WiteBlogPostView.tsx
@@ -5,3 +5,16 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { RiUploadCloudFill } from "react-icons/ri";
 import minhhoa from "../../../images/minhhoa.png";
+
+type Props = {
+    content: any;
+    imageUrl: any;
+    handleEditorChange: any;
+    handleImageChange: any;
+    register: any;
+    handleSubmit: any;
+    onFinish: any;
+    onSubmit: any;
+    errors: any;
+    file: any;
+  };
