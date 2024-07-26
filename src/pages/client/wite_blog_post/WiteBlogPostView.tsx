f7333a70f19e4ae123b212bf91249154e534e56c
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-07-26 10:25:47 +0700

    import fr

---
+++ src/pages/client/wite_blog_post/WiteBlogPostView.tsx
@@ -0,0 +1,7 @@
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { useRef, useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialLight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { RiUploadCloudFill } from "react-icons/ri";
+import minhhoa from "../../../images/minhhoa.png";
