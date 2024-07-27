4002bb5b2d128cefef4ead82114c8df9e997fe6b
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-07-27 20:22:03 +0700

    create component

--- src/components/layouts/LayoutWebsite.tsx
+++ src/components/layouts/LayoutWebsite.tsx
@@ -4,3 +4,8 @@ import Header from "../Header";
 import SideBar from "../SideBar";
 import { Box } from "@mui/material";
 import Footer from "../Footer";
+
+const LayoutWebsite = () => {
+
+}
+export default LayoutWebsite;
