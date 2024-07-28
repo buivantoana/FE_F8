7de9a2d067d04a2fb05e4cfcee7a16c1483a1f6c
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-07-28 08:37:56 +0700

    udate scroll

--- src/components/layouts/LayoutWebsite.tsx
+++ src/components/layouts/LayoutWebsite.tsx
@@ -6,6 +6,29 @@ import { Box } from "@mui/material";
 import Footer from "../Footer";
 
 const LayoutWebsite = () => {
-
+    const [scroll, setScroll] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+          const scrollTop =
+            window.pageYOffset || document.documentElement.scrollTop;
+          const scrollHeight = document.documentElement.scrollHeight;
+          const clientHeight = document.documentElement.clientHeight;
+          const scrollPercentage =
+            (scrollTop / (scrollHeight - clientHeight)) * 100;
+    
+          // Kiểm tra xem người dùng đã cuộn đến khoảng 95% chiều cao của trang chưa
+          if (scrollPercentage >= 85) {
+            setScroll(true);
+          } else {
+            setScroll(false);
+          }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
 }
 export default LayoutWebsite;
