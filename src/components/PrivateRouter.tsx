4a3aaef276609556e87431bd95afae5db3510111
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-08-09 09:36:52 +0700

    create private router

---
+++ src/components/PrivateRouter.tsx
@@ -0,0 +1,8 @@
+import { useLocalStorage } from "@/hooks/useStorage";
+import { Navigate } from "react-router-dom"
+
+const PrivateRouter = ({test,children}:any) =>{
+
+}
+
+export default PrivateRouter
