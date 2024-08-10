5a3b010ba892577195ccb6bf432d7b61f64c9bdc
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-08-10 21:49:02 +0700

    update role permission admin

--- src/components/PrivateRouterAdmin.tsx
+++ src/components/PrivateRouterAdmin.tsx
@@ -2,4 +2,15 @@ import React from 'react';
 import { useLocalStorage } from "@/hooks/useStorage";
 import { Navigate, useLocation } from "react-router-dom";
 
-const rolePermissions:any = {}
+const rolePermissions:any = {
+    admin: [
+        '/dashboard',
+        '/dashboard/courses',
+        '/dashboard/categories',
+        '/dashboard/post',
+        '/dashboard/comment',
+        '/dashboard/contact',
+        '/dashboard/vouchers',
+        '/dashboard/user_vouchers'
+    ],
+}
