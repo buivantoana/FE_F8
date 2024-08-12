db39374ae1b58c31e6d6e3873b2c535de399ccd1
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-08-12 21:04:09 +0700

    update private router admin

--- src/components/PrivateRouterAdmin.tsx
+++ src/components/PrivateRouterAdmin.tsx
@@ -30,3 +30,25 @@ const defaultRedirects:any = {
     course_management: '/dashboard/courses',
     interaction_management: '/dashboard/post'
 };
+const PrivateRouterAdmin = ({ children }: any) => {
+    const [user] = useLocalStorage("user", {});
+    const location = useLocation();
+
+    const currentPath = location.pathname;
+
+    if (Object.keys(user).length === 0) {
+        return <Navigate to="/" />;
+    }
+
+    const allowedPaths = rolePermissions[user.data[0].role] || [];
+
+    if (user.data[0].role === 'admin' || allowedPaths.includes(currentPath)) {
+        return <>{children}</>;
+    }
+
+    const redirectTo = defaultRedirects[user.data[0].role] || '/';
+
+    return <Navigate to={redirectTo} />;
+};
+
+export default PrivateRouterAdmin;
